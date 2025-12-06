17da12b9329d92b9194708751765630912a791d9
bdc3 <ben@23cubed.com>
2025-12-05 19:07:48 -0500

    Update route.ts

--- src/app/api/check-ref-cam/route.ts
+++ src/app/api/check-ref-cam/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from 'next/server';
-import { Director } from '@millicast/sdk';
 import { dev } from '@/lib/dev';
 import { REF_CAM_CONFIG } from '@/dolby/ref-cam-config';
 
 export async function GET() {
   try {
+    const { Director } = await import('@millicast/sdk');
     const data = await Director.getSubscriber({
       streamName: REF_CAM_CONFIG.streamName,
       streamAccountId: REF_CAM_CONFIG.streamAccountId,
