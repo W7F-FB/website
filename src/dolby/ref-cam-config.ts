c91baae8e498206accec8f927862922543d4d13f
bdc3 <ben@23cubed.com>
2025-12-06 15:04:20 -0500

    Ref cam hotfix

--- src/dolby/ref-cam-config.ts
+++ src/dolby/ref-cam-config.ts
@@ -1,7 +1,7 @@
 export const REF_CAM_CONFIG = {
   streamAccountId: 'FBb77b',
   streamName: 'Ref_Cam_Stream',
-  subscriberToken: '48b2e9f09e96b1f82b920b1bf8032282b6ec917e332c8801805f968d032ca93f',
+  subscriberToken: '307812ef947c6d72583d4219e686e4507759da02553e3b3253304a86b942f5d0',
   get iframeUrl() {
     return `https://viewer.millicast.com/?streamId=${this.streamAccountId}/${this.streamName}&token=${this.subscriberToken}`;
   }
